1e0c089c528bd8212f0a737ddf52f03b1e4329a2
Sanghyoun Kim <sanghyou@amazon.com>
2023-10-25 21:52:50 +0900

    modified riches_ingress.yaml with CERTIFICATE_ARN

--- applications/RichesBank/local/k8s/01_deploy_riches.sh
+++ applications/RichesBank/local/k8s/01_deploy_riches.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+export CERTIFICATE_ARN=<CERTIFICATE_ARN>
+
 kubectl apply -f riches_deployment.yaml
 kubectl apply -f riches_service.yaml
-kubectl apply -f riches_ingress.yaml
+#kubectl apply -f riches_ingress.yaml
+cat riches_ingress.yaml | envsubst | kubectl apply -f -
 
 kubectl get pod --namespace riches
 kubectl get service --namespace riches
