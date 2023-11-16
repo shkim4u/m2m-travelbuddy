8016a550e6eb5e2c319e17c9c503cb81b6b9a167
Sanghyoun Kim <sanghyou@amazon.com>
2023-11-16 20:42:25 +0900

    added image id in bootstrap shell file

--- cloud9/bootstrap-v2-with-admin-user-trust.sh
+++ cloud9/bootstrap-v2-with-admin-user-trust.sh
@@ -42,7 +42,7 @@ export SUBNET_ID=`aws ec2 describe-subnets --query "Subnets[?(VpcId==${QUOTED_VP
 
 # 우선 Workshop Studio 콘솔에서 "Get AWS CLI credentials"를 통해 AWS Credentials 환경 변수를 설정한 후 실행할 것.
 #aws cloud9 create-environment-ec2 --name cloud9-workspace --instance-type c5.9xlarge --connection-type CONNECT_SSM --automatic-stop-time-minutes 10080
-aws cloud9 create-environment-ec2 --name cloud9-workspace --instance-type m5.4xlarge --subnet-id "${SUBNET_ID}" --connection-type CONNECT_SSM --automatic-stop-time-minutes 10080
+aws cloud9 create-environment-ec2 --name cloud9-workspace --instance-type m5.4xlarge --image_id amazonlinux-2-x86_64 --subnet-id "${SUBNET_ID}" --connection-type CONNECT_SSM --automatic-stop-time-minutes 10080
 
 
 export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output=text) && echo $AWS_ACCOUNT_ID
